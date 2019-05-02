423abe8e00b84211468d103a11004943540d4a4c
wangkeya <wangky@zpdchina.com>
2019-05-02 13:23:55 +0800

    修改

--- src/goblog/main.go
+++ src/goblog/main.go
@@ -11,8 +11,15 @@ import (
 
 const ROOT = "/Users/alan/goblog"
 
+func init()  {
+	//save pid
+	pidFilename := ROOT + "/pid/" + filepath.Base(os.Args[0]) + ".pid"
+	pid := os.Getpid()
+	ioutil.WriteFile(pidFilename, []byte(strconv.Itoa(pid)), 0755)
+}
+
+
 func main() {
-	savePid()
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -21,11 +28,4 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
-}
-
-//save pid
-func savePid() {
-	pidFilename := ROOT + "/pid/" + filepath.Base(os.Args[0]) + ".pid"
-	pid := os.Getpid()
-	ioutil.WriteFile(pidFilename, []byte(strconv.Itoa(pid)), 0755)
 }
